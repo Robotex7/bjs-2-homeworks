62412392f70a15f9f2c06278a994475200ef10e0
Robotex <ivanivan1001@bk.ru>
2023-11-28 10:40:52 +0300

    Первое задание

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,22 @@
 "use strict"
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+  let discriminant = b ** 2 - 4 * a * c;
+
+  if (discriminant < 0) {
+    return [];
+  } else if (discriminant === 0) {
+    return [-b / (2 * a)];
+  } else {
+    let root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+    let root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+    return [root1, root2];
+  }
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  percent = percent / 100 / 12;
+  let bodyCredit = amount - contribution;
+  let payment = bodyCredit * (percent + (percent / (((1 + percent) ** countMonths) - 1)));
+  let totalAmount = parseFloat((payment * countMonths).toFixed(2));
+  return totalAmount;
 }
