5ba987b5370716b72694d1fdb0eb3b58c0ed463d
Robotex <ivanivan1001@bk.ru>
2023-12-04 19:33:56 +0300

    Второе задание

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -29,19 +29,20 @@ function differenceMaxMinWorker(...arr) {
 }
 
 function differenceEvenOddWorker(...arr) {
-  if (arr.length === 0);{
-    return 0;
-  }
-  let sumEvenElement = 0;
-  let sumOddElement = 0;
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] % 2 === 0) {
-      sumEvenElement += arr[i];
-    } else {
-      sumOddElement += arr[i];
-    }
-  }
-  return sumEvenElement - sumOddElement;
+  if (arr.length === 0) {
+		return 0;
+	}
+	let sumEvenElement = 0;
+	let sumOddElement = 0;
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] % 2 === 0) {
+			sumEvenElement += arr[i];
+		} else {
+			sumOddElement += arr[i];
+		}
+	}
+	return sumEvenElement - sumOddElement;
+
 
 }
 
